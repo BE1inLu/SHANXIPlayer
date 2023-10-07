2424ebf414114abf8ca50b4b8cc50e94e978bb5d
XIDI <enluba2333@gmail.com>
2023-10-07 17:42:09 +0800

    feat: 添加音乐播放方法

---
+++ src/renderer/src/service/musicControl.ts
@@ -0,0 +1,14 @@
+/**
+ * 控制相关
+ * TODO:
+ * 播放队列
+ * 播放模式
+ */
+
+import { playList } from '@renderer/types/default'
+
+class playBar {
+    private playList: playList[] = []
+
+    constructor() {}
+}
